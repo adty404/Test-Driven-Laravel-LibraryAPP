517982c4047d8606e656f104e0f79b119d2cc4f0
Aditya Prasetyo <adty404@gmail.com>
2022-03-09 13:27:29 +0700

    add author resource

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    public function store()
+    {
+        Author::create(request()->only([
+            'name',
+            'dob'
+        ]));
+
+        // return redirect($author->path());
+    }
+
+    // protected function validateRequest()
+    // {
+    //     return request()->validate([
+    //         'name' => 'required',
+    //         'dob' => 'required'
+    //     ]);
+    // }
+}
---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'Aditya',
+            'dob' => '01/14/2000',
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('2000/14/01', $author->first()->dob->format('Y/d/m'));
+    }
+}
